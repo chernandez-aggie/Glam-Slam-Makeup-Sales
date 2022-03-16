bf7472f39943b30eac67b1c7a351e805ae8a39b2
candacehernandez <chernandez.aggie@gmail.com>
2022-03-16 14:51:52 -0500

    I added a test button with a click event listener

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const testButton = document.querySelector("#test");
+testButton.addEventListener("click", handleClick);
+
+function handleClick() {
+    console.log("clicked");
+    // fetch("https://www.fruityvice.com/api/fruit/name")
+    // .then
+}

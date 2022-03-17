2b5456769de141d679815c1027f0c9221534eac5
candacehernandez <chernandez.aggie@gmail.com>
2022-03-17 10:42:20 -0500

    I added a DOMContentLoaded event
    Once content is loaded, a list of makeup products is populated

--- js/index.js
+++ js/index.js
@@ -3,6 +3,24 @@ testButton.addEventListener("click", handleClick);
 
 function handleClick() {
     console.log("clicked");
-    // fetch("https://www.fruityvice.com/api/fruit/name")
-    // .then
+}
+
+document.addEventListener("DOMContentLoaded", pageLanding);
+
+function pageLanding() {
+    fetch("http://makeup-api.herokuapp.com/api/v1/products.json")
+    // ,{
+    //     method: "GET", 
+    //     mode: "no-cors",
+    //     headers: {
+    //         "Content-Type": "application/json",
+    //     },
+    // })
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(element => {
+            const name = document.createElement("p");
+            name.innerHTML = element.name;
+            document.querySelector(".main").appendChild(name);
+        })})
 }

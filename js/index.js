0d891b783709381856dcf06e85022f9711756184
candacehernandez <chernandez.aggie@gmail.com>
2022-03-17 15:33:10 -0500

    I applied general functions to two buttons

--- js/index.js
+++ js/index.js
@@ -1,17 +1,19 @@
-//General Functions
+//********** GENERAL FUNCTIONS **********
+
+//Handle Click Event
 function handleClick(element) {
     const name = document.createElement("p");
     name.innerHTML = element.name;
     document.querySelector(".main").appendChild(name);
 }
-
+//Add Title
 function addTitle(id) {
     const title = document.createElement("p");
     title.innerHTML = id; 
     document.querySelector(".main").appendChild(title);
 }
 
-// ********* BUTTONS **********
+// ********** BUTTONS **********
 
 //Blush
 const blush = document.querySelector("#blush");
@@ -29,7 +31,18 @@ function blushBtn() {
     .then(data => data.forEach(element => handleClick(element))
     )}
 
-
+    const bronzer = document.querySelector("#bronzer");
+    bronzer.addEventListener("click", bronzerBtn);
+    
+    function bronzerBtn() {
+        console.log("clicked");
+        //Add Title
+        let id = "BRONZER";
+        addTitle(id);
+        fetch("http://makeup-api.herokuapp.com/api/v1/products.json?product_type=bronzer")
+        .then(response => response.json())
+        .then(data => data.forEach(element => handleClick(element))
+        )}
 
 // document.addEventListener("DOMContentLoaded", pageLanding);
 

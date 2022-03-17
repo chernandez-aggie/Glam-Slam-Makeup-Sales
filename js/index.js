158415bbcc885a315de3404697fa25c8c1e1bb4f
candacehernandez <chernandez.aggie@gmail.com>
2022-03-17 15:28:00 -0500

    I created general functions for button events

--- js/index.js
+++ js/index.js
@@ -1,27 +1,46 @@
-// Buttons
-const testButton = document.querySelector("#test");
-testButton.addEventListener("click", handleClick);
-
-function handleClick() {
-    console.log("clicked");
+//General Functions
+function handleClick(element) {
+    const name = document.createElement("p");
+    name.innerHTML = element.name;
+    document.querySelector(".main").appendChild(name);
 }
 
+function addTitle(id) {
+    const title = document.createElement("p");
+    title.innerHTML = id; 
+    document.querySelector(".main").appendChild(title);
+}
+
+// ********* BUTTONS **********
+
+//Blush
 const blush = document.querySelector("#blush");
 blush.addEventListener("click", blushBtn);
 
 function blushBtn() {
     console.log("clicked");
-}
+    //Add Title
+    let id = "BLUSH";
+    addTitle(id);
 
-document.addEventListener("DOMContentLoaded", pageLanding);
-
-function pageLanding() {
-    fetch("http://makeup-api.herokuapp.com/api/v1/products.json")
+    //Fetch Blush Product Line
+    fetch("http://makeup-api.herokuapp.com/api/v1/products.json?product_type=blush")
     .then(response => response.json())
-    .then(data => {
-        data.forEach(element => {
-            const name = document.createElement("p");
-            name.innerHTML = element.name;
-            document.querySelector(".main").appendChild(name);
-        })})
-}
+    .then(data => data.forEach(element => handleClick(element))
+    )}
+
+
+
+// document.addEventListener("DOMContentLoaded", pageLanding);
+
+// function pageLanding() {
+//     fetch("http://makeup-api.herokuapp.com/api/v1/products.json")
+//     .then(response => response.json())
+//     .then(data => {
+//         data.forEach(element => {
+//             const name = document.createElement("p");
+//             name.innerHTML = element.name;
+//             document.querySelector(".main").appendChild(name);
+//         })})
+// }
+
